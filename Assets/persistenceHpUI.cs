7da1a5cc13901b1159293daaa3dbd0c01d6ab2bf
Lohan Hébert <lohanhebert7@gmail.com>
2025-11-08 10:29:42 +0100

    fix: heath bar dontDestoyedOnLoad
    la barre de vie ne se reset plus au changement de scène

---
+++ Assets/persistenceHpUI.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class persistenceHpUI : MonoBehaviour
+{
+    public static persistenceHpUI Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
